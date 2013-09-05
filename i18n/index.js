a1c235cffd54bb306d5f14ada68373cfb2d55e12
Raul Vieira <raul.vieira@intelliware.ca>
2013-09-05 10:56:03 -0400

    handling unresolved angular locales

--- i18n/index.js
+++ i18n/index.js
@@ -4,6 +4,8 @@ var path = require('path');
 var ScriptBase = require('../script-base.js');
 var cat = require('shelljs').cat;
 var mkdirp = require('mkdirp');
+var fs = require('fs');
+
 var ngFilePath = path.join(process.cwd(), '/app/bower_components/angular/angular.js');
 
 module.exports = Generator;
@@ -18,14 +20,27 @@ Generator.prototype.createLocalizedAngular = function() {
 
   var localeFilePath = path.join(process.cwd(),
           '/app/bower_components/angular-i18n' + '/angular-locale_' + this.name + '.js'),
-      localizedAngularJSPath = path.join(process.cwd(), '/app/scripts/i18n/angular_' + this.name + '.js');
+      localizedAngularJSPath = path.join(process.cwd(), '/app/scripts/i18n/angular_' + this.name + '.js'),
+      that = this;
 
 
-  mkdirp(path.join(process.cwd(), '/app/scripts/i18n'), function(err) {
+  fs.exists(localeFilePath, function(exists) {
 
-    cat(ngFilePath, localeFilePath).to(localizedAngularJSPath);
+    if(!exists) {
+      console.error('Sorry "%s" isn\'t a locale supported in this version angular', that.name);
+    } else {
+      mkdirp(path.join(process.cwd(), '/app/scripts/i18n'), function(err) {
+        if(err) {
+          console.error('Sorry something unexpected happened.');
+          throw err;
+        } else {
+          cat(ngFilePath, localeFilePath).to(localizedAngularJSPath);
+        }
+      });
+    }
 
   });
+
 
 };
 
