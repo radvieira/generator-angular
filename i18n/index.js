b961ee9a17925a9cc15e0edb7d4f58bc9717b25d
Raul Vieira <raul.vieira@intelliware.ca>
2013-09-05 10:41:45 -0400

    extracting invarient ng javascript path

--- i18n/index.js
+++ i18n/index.js
@@ -4,12 +4,12 @@ var path = require('path');
 var ScriptBase = require('../script-base.js');
 var cat = require('shelljs').cat;
 var mkdirp = require('mkdirp');
+var ngFilePath = path.join(process.cwd(), '/app/bower_components/angular/angular.js');
 
 module.exports = Generator;
 
 function Generator() {
   ScriptBase.apply(this, arguments);
-
 }
 
 util.inherits(Generator, ScriptBase);
@@ -18,14 +18,12 @@ Generator.prototype.createLocalizedAngular = function() {
 
   var localeFilePath = path.join(process.cwd(),
           '/app/bower_components/angular-i18n' + '/angular-locale_' + this.name + '.js'),
+      localizedAngularJSPath = path.join(process.cwd(), '/app/scripts/i18n/angular_' + this.name + '.js');
 
-      ngFilePath = path.join(process.cwd(), '/app/bower_components/angular/angular.js'),
-
-      that = this;
 
   mkdirp(path.join(process.cwd(), '/app/scripts/i18n'), function(err) {
 
-    cat(ngFilePath, localeFilePath).to(path.join(process.cwd(), '/app/scripts/i18n/angular_' + that.name + '.js'));
+    cat(ngFilePath, localeFilePath).to(localizedAngularJSPath);
 
   });
 
